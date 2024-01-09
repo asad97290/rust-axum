d002c9bbefdd1f4fea2df38956dd39cf15c5e046
Asadullah Khan <aukhan@akru.co>
2024-01-09 15:37:15 +0500

    fix version issues

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Router,
+    Json,
+};
+use serde::{Serialize,Deserialize};
+
+#[derive(Serialize)]
+struct User {
+    name: String,
+    age: u8,
+    id: u128
+}
+#[derive(Deserialize)]
+struct UserDto {
+    name: String,
+    age: u8
+}
+
+async fn create_user(
+    Json(payload): Json<UserDto>,
+) -> Result<Json<User>, StatusCode> { // Changed the return type
+    Ok(Json(User {
+        name: payload.name,
+        age: payload.age,
+        id: 12345
+    }))
+}
+
+async fn get_user() -> Result<Json<User>, StatusCode> { // Changed the return type
+    Ok(Json(User {
+        name: "Asad".to_string(),
+        age: 26,
+        id: 12345
+    }))
+}
+
+#[tokio::main]
+async fn main() {
+    // tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/user/getUser", get(get_user))
+        .route("/user/createUser", post(create_user));
+
+    println!("server running on port 3000");
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
